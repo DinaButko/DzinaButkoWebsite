d1df9d2f0f3bf979b8fd7edbde4c643ead7802a8
DzinaButko <butkodina@gmail.com>
2022-01-31 08:45:20 -0500

    Changes routes and views

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Home page. */
-router.get('/', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home',});
 });
 
